384399cc6ac0ef92710655d9f3dfff5512085445
Win Feigle <win.feigle@gmail.com>
2022-10-04 10:42:47 -0600

    Complete module

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Black', 'Nice', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'F', 'White', 'Fake', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'M', 'Brown', 'Mean', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 14, 'F', 'Sugar', 'Mild', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'Grizzly', 'Nice', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', NULL, 'M', 'Black', 'Nice', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'F', 'Brown', 'Vicious', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'M', 'Black', 'Nice', True);
